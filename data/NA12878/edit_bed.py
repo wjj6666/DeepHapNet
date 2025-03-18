0ef1e2a568aa595c2ffc549b78481b1ca95ac526
wjj6666 <1418348139@qq.com>
2025-03-18 11:51:20 +0800

    Delete data directory

--- data/NA12878/edit_bed.py
+++
@@ -1,8 +0,0 @@
-input_path = "/home/wangjiaojiao/dataset/NA12878_HG001_GRCh37/HG001_GRCh37_1_22_v4.2.1_callablemultinter_gt0.bed"
-output_path = "/home/wangjiaojiao/XHap-master/A/data/NA12878/chr21/chr21.bed"
-
-with open(input_path, 'r') as infile, open(output_path, 'w') as outfile:
-    for line in infile:
-        if line.startswith('21\t'):
-            line = 'chr' + line
-            outfile.write(line)
