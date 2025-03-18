aa19abe373b4d932cf2cab56764595f223073740
wjj6666 <1418348139@qq.com>
2025-03-18 11:53:42 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,15 +7,7 @@ from torch.utils.data import Dataset
 from torch.nn import functional as F
 import torch
 
-# get the ACGT statistics of a read matrix
 def ACGT_count(submatrix):
-	"""
-	submatrix:
-		Read-SNV marix (m x n)
-
-	Returns
-		(n x 4) matrix of base counts at each SNP
-	"""
 	out = np.zeros((submatrix.shape[1], 4))
 	for i in range(4):
 		out[:, i] = (submatrix == (i + 1)).sum(axis = 0)
@@ -23,16 +15,6 @@ def ACGT_count(submatrix):
 	return out
 
 def SNVtoHap(SNV_matrix, origin,num_hap: int=2):    
-    """
-    SNV_matrix:
-        Full read-SNV matrix
-    origin: 
-        Specifies origin of each read by an int from (0, 1, ..., num_hap-1)
-        
-    Returns
-        matrix of haplotypes (haplotypes x SNPs)
-    """
-    
     origin_val = np.unique(origin)
     accepted_val = np.arange(num_hap)
     if np.any(np.intersect1d(origin_val, accepted_val) != origin_val):
@@ -57,20 +39,8 @@ def SNVtoHap(SNV_matrix, origin,num_hap: int=2):
                 hap_matrix[h, uncov_pos[j]] = np.random.choice(base_max)
 
     return hap_matrix
-# calculate hamming distance
 
 def HM_distance(read: np.ndarray, 
-	haplo: np.ndarray) -> int:
-    """
-    read:
-        Read denoted by base at each SNP (1-D numpy array)
-	haplo:
-		Haplotype denoted by base at each SNP (1-D numpy array)
-
-	Returns
-		Hamming distance between read and haplotype 
-
-	"""
     if isinstance(read, np.matrix):
         read = read.A1
     if isinstance(haplo, np.matrix):
@@ -85,17 +55,6 @@ def HM_distance(read: np.ndarray,
 
 def MEC(SNV_matrix: np.ndarray,
         hap_matrix: np.ndarray) -> int:  # Compute MEC score
-    
-    """
-	SNV_matrix:
-		Read-SNP matrix
-	hap_matrix:
-		Haplotype-SNP matrix
-
-	Returns
-		MEC score for given SNV matrix and haplotypes
-
-    """
 
     if np.shape(SNV_matrix)[1] != np.shape(hap_matrix)[1]:
         raise ValueError("Different number of SNPs in reads and haplotypes.")
@@ -141,15 +100,6 @@ def read_true_hap(gt_file: str, pos_file: str) -> np.ndarray:
 
 
 def read_hap(hap_file: str) -> np.ndarray:
-    """
-    hap_file:
-        File containing haplotypes
-    
-    Returns
-        k x n numpy array of haplotypes
-    
-    """
-    
     if not path.isfile(hap_file):
         raise OSError("Haplotype file not found.")
     
@@ -170,16 +120,6 @@ def read_hap(hap_file: str) -> np.ndarray:
 
 
 def read_sparseSNVMatrix(mat_file: str) -> csr_matrix:
-    """
-    Read read-SNP matrix into sparse matrix. Useful for large matrices.
-
-    mat_file: str
-        Path to file containing read-SNP matrix
-    
-    Returns
-        csr_matrix: Sparse read-SNP matrix
-    """
-
     with open(mat_file, "r") as f:
         vals, rows, cols = [], [], []
         idx_val_dict = {}
@@ -201,18 +141,6 @@ def read_sparseSNVMatrix(mat_file: str) -> csr_matrix:
     return SNV_matrix
 
 class PermDict:
-    """
-    This is a class to represent permutations when certain indices are interchangeable due to
-    the underlying object. For instance, permutations of [0, 1, 2] and [2, 1, 0] are equivalent
-    on an object [A, C, A].
-    
-    tups: Possible values for permutation indices. Values in same list are interchangeable.
-    pos: # Position in the permutation of interchangeable values
-    
-    For the above example, tups = [[0, 2], [1]], pos = [[0, 2], [1]]. The values [0, 2] are
-    interchangeable and occur at the 0-th and 2nd indices in the permutation.
-    """
-    
     def __init__(self, perm_vals: List[list], pos_idx: List[List], numel: Optional[int]=None):
         self.tups = perm_vals  # Possible values for permutation indices. Values in same tuple are interchangeable
         self.pos = pos_idx  # Position in the permutation of interchangeable values
@@ -223,22 +151,6 @@ class PermDict:
             self.n = numel
     
 def gen_perm(perm: PermDict):  # Generate next possible permutation for given configuration
-    
-    """
-    This is a generator function that generates all possible permutations given a PermDict object.
-    In effect, this generates all possible permutations by interchanging positions where equal
-    values exist in the object on which the permutations are invoked.
-    
-    Parameters
-    ----------
-    perm: PermDict
-        PermDict object to generate permutations from
-    
-    Returns
-    -------
-        NDArray: Permutation
-    """
-    
     gen_list = [permutations(v) for v in perm.tups]
     gen_perm_tup = product(*gen_list)
     for perm_tup in gen_perm_tup:
@@ -249,22 +161,7 @@ def gen_perm(perm: PermDict):  # Generate next possible permutation for given co
         
 def permute_distance(perm1: PermDict, perm2: PermDict
                     ) -> int:
-    """
-    This function computes the distance between the specified permutations.
-    Each permutation of size n is represented as a list of tuples, where each
-    tuple represents positions where the bases/values are identical in the
-    underlying haplotype.
-    
-    Parameters
-    ----------
-    perm1, perm2: PermDict
-        PermDict objects between which to compute distance
-    
-    Returns
-    -------
-    int: Vector distance between closest permutations possible from inputs
-    
-    """
+
     if perm1.n != perm2.n:
         raise ValueError('Permutations are of different sizes.')
         
@@ -277,20 +174,7 @@ def permute_distance(perm1: PermDict, perm2: PermDict
 
 def SWER(recovered_haplo: np.ndarray,
                 true_haplo: np.ndarray) -> float:
-    """
-    Function to compute vector error rate betweeen the recovered and true haplotypes.
-    
-    Parameters
-    ----------
-    recovered_haplo:
-        k x n matrix of recovered haplotypes
-    true_haplo:
-        True haplotypes (ground truth)
-    
-    Returns
-    -------
-        float: Vector error rate for the polyploid case, switch error rate for diploids
-    """
+
     
     if np.shape(recovered_haplo) != np.shape(true_haplo):
         raise ValueError("Input arguments should have the same shape.")
@@ -322,7 +206,7 @@ def SWER(recovered_haplo: np.ndarray,
                    for i in range(len(unique_vals))]
         perm_tups = [list(perm_best[np.nonzero(inverse_idx == i)])
                      for i in range(len(unique_vals))]       
-#         print(perm_best, perm_tups, pos_idx)
+
         if dis_min < n_hap:  # Ignore if there is no good match
             PermDict_list.append(PermDict(perm_tups, pos_idx, n_hap))
         else:
@@ -336,41 +220,29 @@ def SWER(recovered_haplo: np.ndarray,
     res = vec_err/(n_hap*(n_SNP - 1))
     if n_hap == 2:  # Compute switching error in case of two haplotypes
         res = res/2
-    
-    # print("Number of mismatched SNPs: %d" %len(mismatch_SNP))
-    # print(mismatch_SNP)
+
     return res
 
 
 def save_ckp(state, checkpoint_path):  
-    """
-    state: checkpoint we want to save
-    checkpoint_path: path to save checkpoint
-    """
+
     f_path = checkpoint_path  # Save path
     torch.save(state, f_path)
 
 def load_ckp(checkpoint_path, model, optimizer):
-    """
-    checkpoint_path: path to save checkpoint
-    model: model that we want to load checkpoint parameters into       
-    optimizer: optimizer we defined in previous training
-    """
+
     checkpoint = torch.load(checkpoint_path)
-    # initialize state_dict from checkpoint to model
+
     model.load_state_dict(checkpoint['state_dict'])
-    # initialize optimizer from checkpoint to optimizer
+
     optimizer.load_state_dict(checkpoint['optimizer'])
 
-    # return model, optimizer, epoch value
     return model, optimizer, checkpoint['epoch']
 
 
 class SNVMatrixDataset(Dataset):
     def __init__(self, SNV_file, transform=None):
-        """
-        SNV_file: txt file containing SNV matrix
-        """
+
         SNV_matrix_raw = np.loadtxt(SNV_file, dtype=int)
         self.SNV_matrix = SNV_matrix_raw[np.sum(SNV_matrix_raw != 0, axis=1) > 1]
 	 
@@ -382,14 +254,12 @@ class SNVMatrixDataset(Dataset):
         SNV_row_onehot = F.one_hot(SNV_row, 5)[:,1:]
         SNV_row_onehot = SNV_row_onehot.type(torch.float32)
         SNV_row_onehot = SNV_row_onehot.transpose(1,0)
-        return SNV_row_onehot[None,:], idx  # Shape is batch x 4 x numSNP
+        return SNV_row_onehot[None,:], idx  
 
-# Dataset class to use if SNV matrix is stored in sparse format
+
 class SparseSNVMatrixDataset(Dataset):
     def __init__(self, SNV_matrix, transform=None):
-        """
-        SNV_file: Sparse/dense read-SNP matrix
-        """
+
         self.SNV_matrix = csr_matrix(SNV_matrix)
 	 
     def __len__(self):
@@ -404,11 +274,7 @@ class SparseSNVMatrixDataset(Dataset):
 
 
 def chunk_data(datapath, chunk_size=1000, overlap_frac=0.5):
-    """
-    Chunk read-SNP matrix into blocks of size chunk_size shifted by 
-    overlap_frac*chunk_size SNPs. 
-      
-    """
+
     with open(datapath, "r") as f:
         vals, rows, cols = [], [], []
         idx_val_dict = {}
